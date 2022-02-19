6c2bd034c9de894aff77fe12a1ec63460c36096f
viet nguyen <vietnguyen@noreply>
2022-02-19 10:55:39 +0100

    Deploy to docs to subdomain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://tacos.openbeta.io',
-  baseUrl: '/docs/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
